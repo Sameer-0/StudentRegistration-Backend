530e4ad747363e2a5d3b0c87764466cbbdf79707
Sameer-0 <sameershaikhx123@gmail.com>
2022-06-13 12:23:31 +0530

    JUNE 13 12:15PM

--- src/main/java/com/userregistration/springboot/controller/UserController.java
+++ src/main/java/com/userregistration/springboot/controller/UserController.java
@@ -9,29 +9,29 @@ import java.util.List;
 
 /* Created by Arjun Gautam */
 @RestController
-@CrossOrigin("https://springboot-react-application-v.herokuapp.com")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("user")
+    @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
 
-    @GetMapping("users")
+    @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @GetMapping("user/{id}")
+    @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @PutMapping("user/{id}")
+    @PutMapping("/user/{id}")
     User updateUser(@RequestBody User newUser, @PathVariable Long id) {
         return userRepository.findById(id)
                 .map(user -> {
@@ -42,7 +42,7 @@ public class UserController {
                 }).orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @DeleteMapping("user/{id}")
+    @DeleteMapping("/user/{id}")
     String deleteUser(@PathVariable Long id){
         if(!userRepository.existsById(id)){
             throw new UserNotFoundException(id);
